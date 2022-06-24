dab5fb6185dcf32d128ea40949d0bbb2362af1f5
krishnakrmahto97 <krish.thorcode@gmail.com>
2022-06-24 15:52:30 +0530

    added code + config to register product-service as eureka client

---
+++ product-service/src/test/java/com/sampleprojects/ecommerce/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampleprojects.ecommerce.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
